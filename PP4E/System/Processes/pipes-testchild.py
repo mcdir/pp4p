e57a8cc164bc8c9eadeb2bcb22ddffb41247c5ae
mcdir <mc_dir@mail.ru>
2015-01-03 16:33:49 +0300

    Update pipes-testchild.py
    may be raw_input()
    because with input() die:
    Traceback (most recent call last):
      File "pipes.py", line 36, in <module>
        reply = input()  # from child's stdout
      File "<string>", line 1
        Child 8251 got: [('Hello 1 from parent', 8250)]

--- PP4E/System/Processes/pipes-testchild.py
+++ PP4E/System/Processes/pipes-testchild.py
@@ -5,7 +5,7 @@ sys.stderr.write('Child %d of %d got arg: "%s"\n' %
                                 (mypid, parentpid, sys.argv[1]))
 for i in range(2):
     time.sleep(3)              # make parent process wait by sleeping here
-    recv = input()             # stdin tied to pipe: comes from parent's stdout
+    recv = raw_input()             # stdin tied to pipe: comes from parent's stdout
     time.sleep(3)
     send = 'Child %d got: [%s]' % (mypid, recv)
     print(send)                # stdout tied to pipe: goes to parent's stdin
